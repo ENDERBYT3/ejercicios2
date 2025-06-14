f8ce5fa1cc1b4b46d5840ef56ae8fc47226c0c41
ENDERBYT3 <endrsanchez.050@gmail.com>
2025-06-14 11:32:46 -0500

    importe el archivo funcion

---
+++ principal.py
@@ -0,0 +1,8 @@
+from funcion.funcion import sumar, multiplicar
+def main():
+    a = 5
+    b = 10
+    print(f"La suma de {a} y {b} es: {sumar(a, b)}")
+    print(f"La multiplicación de {a} y {b} es: {multiplicar(a, b)}")
+if __name__ == "__main__":
+    main()
